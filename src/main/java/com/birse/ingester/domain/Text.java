4c32c3422de74fd860e292db45985b8e7b130471
adambirse <adambirse@gmail.com>
2019-02-04 07:28:59 +0000

    Add support for LocalDateTime

--- src/main/java/com/birse/ingester/domain/Text.java
+++ src/main/java/com/birse/ingester/domain/Text.java
@@ -1,5 +1,10 @@
 package com.birse.ingester.domain;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+
 import java.time.LocalDateTime;
 
 /**
@@ -8,7 +13,11 @@ import java.time.LocalDateTime;
 public class Text {
 
     private Long id;
+
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
+    @JsonDeserialize(using = LocalDateTimeDeserializer.class)
     private LocalDateTime time;
+
     private String text;
 
     public Text() {
@@ -16,6 +25,12 @@ public class Text {
     }
     public Text(String text) {
         id = RandomIDGenerator.generateLong();
+        this.text = text;
+    }
+
+    public Text(Long id, LocalDateTime time, String text) {
+        this.id = id;
+        this.time = time;
         this.text = text;
     }
 
